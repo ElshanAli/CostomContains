0c33df7db3b3c006c27524bcced4aea3dd101a38
ElshanAli <elshanml@code.edu.az>
2022-05-03 20:03:44 +0400

    Added Costom Contains Task

---
+++ CostomContains/Extention.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CostomContains
+{
+   public static class Extention
+    {
+        public static bool MyCostomContains(this string sentence, string part)
+        {
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i]== part[0])
+                {
+                    for (int j = 0; j < part.Length; j++)
+                    {
+                        if (part[j]!=sentence[i+j])
+                        {
+                            break;
+                        }
+                        if (j==part.Length-1)
+                        {
+                            return true;
+                        }
+                
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ CostomContains/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CostomContains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter sentence:");
+            string sentence = Console.ReadLine().ToLower();
+            Console.WriteLine("Enter the part which you want:");
+            string part = Console.ReadLine().ToLower();
+
+            Console.WriteLine(sentence.MyCostomContains(part));
+            
+        }
+    }
+}
